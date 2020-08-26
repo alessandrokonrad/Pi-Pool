f6b82bc5415f7a94e9eca703a83ce00dcfdfafbd
alessandrokonrad <35738310+alessandrokonrad@users.noreply.github.com>
2020-08-26 15:05:01 +0200

    Create start.sh

---
+++ scripts/fullGuide/start.sh
@@ -0,0 +1,21 @@
+# Execute this file from $HOME
+
+echo "Updating Ubuntu"
+echo
+sudo apt-get update
+sudo apt-get upgrade
+echo
+echo "Installing dependencies"
+echo
+sudo apt-get install libsodium-dev build-essential pkg-config libffi-dev libgmp-dev libssl-dev libtinfo-dev libsystemd-dev zlib1g-dev make g++ tmux git jq wget libncursesw5 llvm -y
+echo
+echo "Installing Haskell platform"
+echo
+sudo apt-get install -y haskell-platform
+wget https://github.com/alessandrokonrad/Pi-Pool/raw/master/aarch64/cabal3.2/cabal
+chmod +x cabal
+mkdir -p ~/.local/bin
+mv cabal ~/.local/bin
+sudo rm /usr/bin/cabal
+echo 'export PATH="~/.local/bin:$PATH"' >> .bashrc
+rm -f start.sh

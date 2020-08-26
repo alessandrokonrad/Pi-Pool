64e75774fac1b6feb6190a54c77d9574876b189f
alessandrokonrad <35738310+alessandrokonrad@users.noreply.github.com>
2020-08-26 16:10:33 +0200

    Create installCardano.sh

---
+++ scripts/fullGuide/installCardano.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+echo "Cloning Cardano repository"
+echo
+git clone https://github.com/input-output-hk/cardano-node.git ~/cardano-node
+cd ~/cardano-node
+echo -e "package cardano-crypto-praos\n  flags: -external-libsodium-vrf" > cabal.project.local
+git fetch --all --tags
+echo "Checking out version 1.19.0"
+git checkout tags/1.19.0
+echo "Building node, this takes a while..."
+cabal build all
+cp -p ~/cardano-node/dist-newstyle/build/aarch64-linux/ghc-8.6.5/cardano-node-1.19.0/x/cardano-node/build/cardano-node/cardano-node ~/.local/bin/
+cp -p ~/cardano-node/dist-newstyle/build/aarch64-linux/ghc-8.6.5/cardano-cli-1.19.0/x/cardano-cli/build/cardano-cli/cardano-cli ~/.local/bin/
+cd
+echo "Done!"
